b9815c060e0dc352341d7d47d4eabd1e9a41f992
Wil <wiltemes1@gmail.com>
2023-11-28 12:50:24 +0200

    zeev bug fixed

--- app/src/main/java/com/example/drushim_alpha/AlarmReciver.java
+++ app/src/main/java/com/example/drushim_alpha/AlarmReciver.java
@@ -13,7 +13,7 @@ public class AlarmReciver extends BroadcastReceiver  {
     public void onReceive(Context context, Intent intent) {
         String etNotiWrite = intent.getStringExtra("notification");
         NotificationHelper.showNotificationBtn(context,etNotiWrite);
-        Toast.makeText(context,"Notification set successfully",Toast.LENGTH_SHORT).show();
+        Toast.makeText(context,"Notification pushed successfully",Toast.LENGTH_SHORT).show();
 
     }
 }
